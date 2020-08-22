f33ff4bebd6d01fc38575aed5b05d1b04cf2a131
Erick <erick.bessa96@gmail.com>
2020-08-22 13:11:34 -0300

    First commit

---
+++ FormatTextForJasomn/FrmPrincipal.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace FormatTextForJasomn
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTextInput = new System.Windows.Forms.TextBox();
+            this.btnFormatText = new System.Windows.Forms.Button();
+            this.txtLog = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTextInput
+            // 
+            this.txtTextInput.Dock = System.Windows.Forms.DockStyle.Top;
+            this.txtTextInput.Location = new System.Drawing.Point(0, 0);
+            this.txtTextInput.Multiline = true;
+            this.txtTextInput.Name = "txtTextInput";
+            this.txtTextInput.Size = new System.Drawing.Size(800, 288);
+            this.txtTextInput.TabIndex = 0;
+            // 
+            // btnFormatText
+            // 
+            this.btnFormatText.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnFormatText.Location = new System.Drawing.Point(646, 325);
+            this.btnFormatText.Name = "btnFormatText";
+            this.btnFormatText.Size = new System.Drawing.Size(142, 89);
+            this.btnFormatText.TabIndex = 1;
+            this.btnFormatText.Text = "Formatar";
+            this.btnFormatText.UseVisualStyleBackColor = true;
+            this.btnFormatText.Click += new System.EventHandler(this.btnFormatText_Click);
+            // 
+            // txtLog
+            // 
+            this.txtLog.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.txtLog.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtLog.Location = new System.Drawing.Point(3, 16);
+            this.txtLog.Multiline = true;
+            this.txtLog.Name = "txtLog";
+            this.txtLog.Size = new System.Drawing.Size(311, 121);
+            this.txtLog.TabIndex = 2;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtLog);
+            this.groupBox1.Location = new System.Drawing.Point(2, 298);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(317, 140);
+            this.groupBox1.TabIndex = 3;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Log";
+            // 
+            // FrmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnFormatText);
+            this.Controls.Add(this.txtTextInput);
+            this.Name = "FrmPrincipal";
+            this.Text = "FormatTextEbs";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtTextInput;
+        private System.Windows.Forms.Button btnFormatText;
+        private System.Windows.Forms.TextBox txtLog;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ FormatTextForJasomn/FrmPrincipal.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FormatTextForJasomn
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+            AddLog("Insira a legenda completa...");
+        }
+
+        private void btnFormatText_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (txtTextInput.Text == "")
+                    return;
+
+                string sText = txtTextInput.Text;
+                char[] trimChars = { '"', '(', ')', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ':' };
+                for (int i = 0; i < trimChars.Length; i++)
+                {
+                    sText = sText.Replace(trimChars[i], '$');
+
+                }
+
+                sText = sText.Replace("$", "");
+                sText = sText.Replace("\r", "");
+                sText = sText.Replace("\n", "");
+                sText = sText.Replace("\r\n", "");
+
+
+                string sPathFile = Directory.GetCurrentDirectory() + @"\BodyJson.txt";
+                if (File.Exists(sPathFile))
+                    File.Delete(sPathFile);
+
+                using (StreamWriter stw = new StreamWriter(sPathFile))
+                {
+                    stw.WriteLine(sText);
+                    stw.AutoFlush = true;
+                    stw.Close();
+                }
+
+                AddLog("Arquivo gerado, caminho:" + sPathFile);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(":(", "Erro: " + ex.Message, MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public void AddLog(string Line)
+        {
+            string sMili = "000" + DateTime.Now.Millisecond.ToString();
+            sMili = sMili.Substring(sMili.Length - 3, 3);
+            string sLinha = String.Format("{0:HH:mm:ss}", DateTime.Now) + "." + sMili + " - " + Line;
+
+            if (txtLog.Lines.Length > 10)
+                txtLog.Clear();
+            txtLog.Text += sLinha.Replace("\0", "") + "\r\n";
+            if (txtLog.Text.Length > 0)
+            {
+                txtLog.Select(txtLog.Text.Length - 1, 0);
+                txtLog.ScrollToCaret();
+            }
+        }
+    }
+}
